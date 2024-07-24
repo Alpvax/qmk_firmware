a41df12692f366ed0b2b1f32ba56052d3dcea049
Alpvax <development@alpvax.uk>
2024-07-24 21:27:03 +0100

    Fix logo, update font, fix R master not working

--- keyboards/sofle/keymaps/alpvax/config.h
+++ keyboards/sofle/keymaps/alpvax/config.h
@@ -6,7 +6,7 @@
 see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
 for more options.
 */
-// #define MASTER_LEFT
+// #define MASTER_RIGHT
 #define EE_HANDS
 
 // custom transport for displaying master version on both side
